c53c78b82bdf49f22da9c60c001ab22798108ea3
rajan <rajanbhateja6@gmail.com>
2025-04-06 23:27:48 +0530

    Modified dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,16 @@
 import streamlit as st
-from Demos.win32console_demo import window_size
-
 from scraper import extract_article_text
 
 st.title('News Summarizer and Sentiment Analyzer')
 st.header('Get summarized news articles')
-st.set_option(window_size='wide')
 
 col1, col2, col3 = st.columns(3)
 global full_text
 
 with col1:
     with st.form(key='form'):
-        url = st.text_input('Enter URL:', placeholder='URL should be a Times of India URL')
-        submit = st.form_submit_button('Submit', 'secondary')
+        url = st.text_input('Enter URL:')
+        submit = st.form_submit_button('Submit')
 
         if submit and url:
             full_text = extract_article_text(url)
